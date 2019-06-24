94f1f264d3f98cf3693acb50deaef90d3f2db09b
AJARTCGA <ajartcga@gmail.com>
2019-06-24 09:47:34 -0400

    Initial work done on selection analyze app modifications. Testing still needed

---
+++ apps/SelectionAnalyze/SelectionAnalyze.cc
@@ -0,0 +1,147 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2019.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//  Analyze probabilities for selection using various selection techniques.
+
+// Empirical Includes
+#include "config/config.h"
+#include "config/ArgManager.h"
+#include "config/command_line.h"
+#include "base/vector.h"
+#include "tools/IndexMap.h"
+
+// Local Includes
+#include "SelectionAnalyzeConfig.h"
+#include "SelectionData.h"
+
+
+int main(int argc, char* argv[]){
+    auto args = emp::cl::ArgManager(argc, argv);
+    SelectionAnalyzeConfig config;
+    std::string config_filename = "config.cfg";
+    config.Read(config_filename);
+    if (args.ProcessConfigOptions(config, std::cout, config_filename, "config-macros.h") == false)
+        exit(-1);
+    if (args.TestUnknown() == false){
+        std::cerr << "Unknown arguments detected!" << std::endl;
+        exit(-1); // If there are leftover args, throw an error.
+    } 
+
+    // Load in general, non selection scheme dependent config options    
+    SelectionScheme selection_scheme = (SelectionScheme)config.SELECTION_SCHEME();
+    std::string input_filename = config.INPUT_FILENAME(); 
+    std::string output_filename = config.OUTPUT_FILENAME(); 
+    bool verbose = config.VERBOSE(); 
+    bool remove_headers = !config.NO_COL_HEADINGS();  
+    if(verbose) std::cout << "Config loaded successfully!" << std::endl;
+    
+    // Load in the data
+    if(input_filename.length() <= 0){
+        std::cerr << "Error! No input filename set. INPUT_FILENAME must be set in the config!" 
+            << std::endl;
+        exit(-1);
+    }
+    std::ofstream out_file;
+    if (output_filename.length() > 0) {
+        std::cout << "Opening output file: " << output_filename << std::endl;
+        out_file.open(output_filename);
+    }
+    std::ostream & out_stream = (output_filename.length() > 1) ? out_file : std::cout;
+ 
+            
+    // Handle the different selection schemes independently
+    switch(selection_scheme){
+        case LEXICASE:{
+            // Load lexicase-specific configuration options
+            size_t start_idx = config.LEXICASE_START_IDX();
+            if(verbose) std::cout << "Loading in file: "  << input_filename << std::endl;
+            SelectionData data(input_filename, start_idx, remove_headers);
+            if(verbose) data.SetVerbose();
+            bool do_subsampling = config.LEXICASE_DO_SUBSAMPLING();
+            if(!do_subsampling){
+                // Calculate probabilities! (Full lexicase) 
+                data.AnalyzeLexicase();
+                data.CalcLexicaseProbs();
+                data.PrintSelectProbs(out_stream, false);  
+            }
+            else{
+                // Estimate probabilities! (Subsampled lexicase)
+                size_t sub_pop_count = config.LEXICASE_SUBSAMPLING_GROUP_SIZE();
+                size_t sub_test_count = config.LEXICASE_SUBSAMPLING_TEST_COUNT();
+                size_t sub_trial_count = config.LEXICASE_SUBSAMPLING_NUM_SAMPLES();
+                emp::Random random;
+                auto result = data.CalcSubsampleLexicaseProbs(sub_pop_count, 
+                    sub_test_count, sub_trial_count, random);
+                double total = 0.0;
+                for (size_t i = 0; i < result.size(); i++) {
+                  if (i) out_stream << ',';
+                  out_stream << result[i];
+                  total += result[i];
+                }
+                out_stream << std::endl;
+                std::cout << "Total prob = " << total << std::endl;
+            }
+        }
+        break;
+        case TOURNAMENT:{
+            // Load in aggregate fitness data
+            size_t agg_fit_idx = config.AGGREGATE_FIT_IDX();
+            if(verbose) std::cout << "Loading in file: "  << input_filename << std::endl;
+            SelectionData data(input_filename, 0, remove_headers);
+            if(verbose) data.SetVerbose();
+            data.SetFitnessID(agg_fit_idx);
+            // Load in tournament-specific configuration options
+            size_t tourney_size = config.TOURNAMENT_SIZE(); 
+            size_t tourney_trial_count = config.TOURNAMENT_TRIALS(); 
+            // Tournament is just elite with a sampled population
+            // Thus, tournamnet is just lexicase with one column and pop sampling!
+            emp::Random random;
+            auto result = data.CalcSubsampleLexicaseProbs(tourney_size,
+                 data.GetNumCriteria(), tourney_trial_count, random);
+            double total = 0.0;
+            for (size_t i = 0; i < result.size(); i++) {
+              if (i) out_stream << ',';
+              out_stream << result[i];
+              total += result[i];
+            }
+            out_stream << std::endl;
+            std::cout << "Total prob = " << total << std::endl;
+        }
+        break;
+        case ELITE:{
+            // Load in aggregate fitness data
+            size_t agg_fit_idx = config.AGGREGATE_FIT_IDX();
+            if(verbose) std::cout << "Loading in file: "  << input_filename << std::endl;
+            SelectionData data(input_filename, 0, remove_headers);
+            if(verbose) data.SetVerbose();
+            data.SetFitnessID(agg_fit_idx);
+            
+            // Elite is just lexicase on one column!
+            data.AnalyzeLexicase();
+            data.CalcLexicaseProbs();
+            data.PrintSelectProbs(out_stream, false);  
+        }
+        break;
+        case ROULETTE:{
+            // Load in aggregate fitness data
+            size_t agg_fit_idx = config.AGGREGATE_FIT_IDX();
+            if(verbose) std::cout << "Loading in file: "  << input_filename << std::endl;
+            SelectionData data(input_filename, 0, remove_headers);
+            if(verbose) data.SetVerbose();
+            data.SetFitnessID(agg_fit_idx);
+            //Calculate probabilities using roulette selection
+            emp::vector<double> fit_data = data.GetFitData();
+            const size_t num_orgs = data.GetNumOrgs();
+            emp::IndexMap fit_map(num_orgs);
+            for (size_t i = 0; i < num_orgs; i++) {
+              fit_map[i] = fit_data[i];
+            }
+            for (size_t i = 0; i < num_orgs; i++) {
+              out_stream << fit_map.GetProb(i) << " ";
+            }
+            out_stream << std::endl;
+        }
+        break;
+    }
+}
---
+++ apps/SelectionAnalyze/SelectionAnalyzeConfig.h
@@ -0,0 +1,57 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2019.
+//  Released under the MIT Software license; see doc/LICENSE
+//
+//  Analyze probabilities for selection using various selection techniques.
+
+// Empirical Includes
+#include "config/config.h"
+
+enum SelectionScheme{
+    LEXICASE = 0,
+    TOURNAMENT = 1, 
+    ELITE = 2, 
+    ROULETTE = 3
+};
+
+EMP_BUILD_CONFIG(SelectionAnalyzeConfig,
+    // General Settings
+    GROUP(GENERAL, "General settings that apply to all selection schemes"),
+    VALUE(SELECTION_SCHEME, size_t, 0, "The type of selection to be analyzed. 0 for lexicase, 1 for "
+        "tournament, 2 for elite, 3 for roulette."),
+    VALUE(INPUT_FILENAME, std::string, "", "The path of the file containing the data to be used."),
+    VALUE(OUTPUT_FILENAME, std::string, "", "The path to a file that will be created to save the "
+        "generated selection probabilities."),
+    VALUE(NO_COL_HEADINGS, bool, false, "Set to true if column headers have already been "
+        "removed."),
+    VALUE(VERBOSE, bool, false, "Prints more information during computation, useful for debugging."),
+    
+    
+    // Aggregate Schemes Settings
+    GROUP(AGGREGATE, "Settings that apply to all selection schemes that aggregate fitness across "
+        " test cases (i.e., not lexicase selection)."),
+    VALUE(AGGREGATE_FIT_IDX, size_t, 0, "Index of the column that contains aggregate fitness values "
+        "to be used in selection."),
+    
+    // Lexicase Settings
+    GROUP(LEXICASE, "Settings that apply only to lexicase selection (SELECTION_SCHEME = 0)."),
+    VALUE(LEXICASE_START_IDX, size_t, 0, "Index of the column (starting at zero) that represents "
+        "a test case to be used in lexicase. Note: There should be no columns following the test "
+        "case columns."), 
+    VALUE(LEXICASE_DO_SUBSAMPLING, bool, false, "If 1, LEXICASE_POP_SAMPLING, LEXICASE_TEST_SAMPLING"
+        " and LEXICASE_SUBSAMPLING_SAMPLES will be used. Note: this is an estimated analysis."),
+    VALUE(LEXICASE_SUBSAMPLING_GROUP_SIZE, size_t, 0, "How many individuals will be sampled before"
+        " doing lexicase selection. For cohort selection, this is cohort size. (a value of zero"
+        " gives the whole population (such as in downsampled lexicase)."), 
+    VALUE(LEXICASE_SUBSAMPLING_TEST_COUNT, size_t, 0, "Number of tests to sample before running "
+        " lexicase selection."),
+    VALUE(LEXICASE_SUBSAMPLING_NUM_SAMPLES, size_t, 1000, "Number of times to sample the"
+        " configuration. The larger the number, the closeer the value the closer the estimate should"
+        " be to the true value."),    
+    // Tournament Settings
+    GROUP(TOURNAMENT, "Settings that apply only to tournament selection (SELECTION_SCHEME = 1)."),
+    VALUE(TOURNAMENT_SIZE, size_t, 0, "Number of organisms in each tournament."
+        " (0 for whole population)"), 
+    VALUE(TOURNAMENT_TRIALS, size_t, 0, "Number of organisms in each tournament."
+        " (0 for whole population)") 
+)
